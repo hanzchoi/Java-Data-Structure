296cf4f28b16eed45566e440bd54de74b6a241f3
personabe1984 <personabe1984@gmail.com>
2016-04-10 14:53:12 -0400

    first commit

---
+++ src/nyc/personabe1984/treehouse/Main.java
@@ -0,0 +1,11 @@
+package nyc.personabe1984.treehouse;
+
+import com.teamtreehouse.Treet;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Treet treet = new Treet();
+        System.out.printf("This is a new Treet: %s %n", treet);
+    }
+}
