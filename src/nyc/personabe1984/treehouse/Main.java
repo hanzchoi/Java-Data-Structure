5ae03a71842f2d445653c14a7d127d7fcfb7c052
personabe1984 <personabe1984@gmail.com>
2016-04-15 18:50:49 -0400

    overrided the toString method from the object class

--- src/nyc/personabe1984/treehouse/Main.java
+++ src/nyc/personabe1984/treehouse/Main.java
@@ -2,10 +2,12 @@ package nyc.personabe1984.treehouse;
 
 import com.teamtreehouse.Treet;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	    Treet treet = new Treet();
+	    Treet treet = new Treet("","", new Date());
         System.out.printf("This is a new Treet: %s %n", treet);
     }
 }
