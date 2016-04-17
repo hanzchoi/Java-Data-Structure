09f019612d0abc876186410ef37a9b1caef9803f
personabe1984 <personabe1984@gmail.com>
2016-04-17 14:02:50 -0400

    learned about comparing strings and other objects

--- src/nyc/personabe1984/treehouse/Main.java
+++ src/nyc/personabe1984/treehouse/Main.java
@@ -2,6 +2,7 @@ package nyc.personabe1984.treehouse;
 
 import com.teamtreehouse.Treet;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -14,10 +15,20 @@ public class Main {
                 "@treehouse course about data structure",
                 new Date(1421849732000L)
         );
+
+        Treet secondTreet = new Treet(
+                "journeytocode",
+                "@treehouse makes learning Java sooooo fun! #treet",
+                new Date(1421878767000L)
+        );
+
         System.out.printf("This is a new Treet: %s %n", treet);
         System.out.println("The words are:");
         for (String word : treet.getWords()){
             System.out.println(word);
         }
+
+        Treet[] treets = {treet, secondTreet};
+        Arrays.sort(treets);
     }
 }
