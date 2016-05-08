13666c3c96dbea80953cb8aefa02b45bd8cc66a9
personabe1984 <personabe1984@gmail.com>
2016-05-08 00:11:42 -0400

    making a Karaoke machine

---
+++ src/Karaoke.java
@@ -0,0 +1,19 @@
+import model.Song;
+import model.SongBook;
+
+public class Karaoke {
+
+    public static void main(String[] args){
+        Song song = new Song(
+                "Michael Jackson",
+                "Beat It",
+                "https://www.youtube.com/watch?v=SaEC9i9QOvk"
+        );
+
+        SongBook songBook = new SongBook();
+        System.out.printf("Adding %s %n",song);
+        songBook.addSong(song);
+        System.out.printf("There are %d songs.", songBook.getSongCount()); //%d is for the nubers
+    }
+
+}
