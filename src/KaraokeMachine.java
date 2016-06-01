6dc3ee5142a6f924fc67035c3440254b2c32c274
Hans Choi <personabe1984@gmail.com>
2016-06-01 15:27:22 -0400

    Added a play segment of the menu

--- src/KaraokeMachine.java
+++ src/KaraokeMachine.java
@@ -4,21 +4,21 @@ import treehouse.model.SongBook;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class KaraokeMachine {
     private SongBook mSongBook;
     private BufferedReader mReader;
+    private Queue<Song> mSongQueue;
     private Map<String, String> mMenu;
 
     public KaraokeMachine(SongBook songBook){
         mSongBook = songBook;
         mReader = new BufferedReader(new InputStreamReader(System.in));
+        mSongQueue = new ArrayDeque<Song>();
         mMenu = new HashMap<>();
         mMenu.put("add", "Add a new song to the song book");
+        mMenu.put("play", "Play next song in the queue");
         mMenu.put("choose", "Choose a song to sing! ");
         mMenu.put("quit", "Give up. Exit the program");
     }
@@ -39,8 +39,11 @@ public class KaraokeMachine {
                     case "choose":
                         String artist = promptArtist();
                         Song artistSong = promptSongForArtist(artist);
-                        // TODO: add to a song queue
+                        mSongQueue.add(artistSong);
                         System.out.printf("You chose: %s %n ", artistSong);
+                        break;
+                    case "play":
+                        playNext();
                         break;
                     case "quit":
                         System.out.println("Thanks for playing!");
@@ -58,8 +61,9 @@ public class KaraokeMachine {
     }
 
     private String promptAction() throws IOException{
-        System.out.printf("There are %d songs available. Your option are: %n",
-                          mSongBook.getSongCount());
+        System.out.printf("There are %d songs available and %d in the queue. Your option are: %n",
+                          mSongBook.getSongCount(), mSongQueue.size());
+
         for(Map.Entry<String, String> option : mMenu.entrySet()){
             System.out.printf("%s - %s %n",
                               option.getKey(),
@@ -96,6 +100,7 @@ public class KaraokeMachine {
         for(Song song : songs){
             songTitles.add(song.getTitle());
         }
+        System.out.printf("Available songs for %s: %n", artist);
         int index = promptForIndex(songTitles);
         return songs.get(index);
     }
@@ -106,10 +111,23 @@ public class KaraokeMachine {
             System.out.printf("%d.)  %s %n", counter, option);
             counter++;
         }
+        System.out.print("Your choice:  ");
         String optionAsString = mReader.readLine();
         int choice = Integer.parseInt(optionAsString.trim());
-        System.out.print("Your choice:  ");
         return choice - 1;
+    }
+
+    public void playNext(){
+        Song song = mSongQueue.poll();
+        if(song == null){
+            System.out.println("Sorry there are no songs in the queue." +
+                    " Use choose from the menu to add some.");
+        }else{
+            System.out.printf("%n%n%n Open %s to hear %s by %s %n%n%n",
+                    song.getVideoUrl(),
+                    song.getTitle(),
+                    song.getArtist());
+        }
     }
 
 }
